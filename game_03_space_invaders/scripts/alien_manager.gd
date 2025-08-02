8470b2d0023cbd9d3fac4ceae0e63d9c923f7f7a
JermWay <jermway@gmail.com>
2025-08-01 19:27:24 -0500

    Speed up as aliens are destroyed

--- game_03_space_invaders/scripts/alien_manager.gd
+++ game_03_space_invaders/scripts/alien_manager.gd
@@ -5,18 +5,23 @@ signal alien_moved
 @export var alien_scene: PackedScene
 @onready var alien_group: Node2D = $AlienGroup
 @onready var projectile_manager: Node2D = $"../ProjectileManager"
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var timer: Timer = $Timer
 
 var direction := 1
 var is_at_edge := false
 var alien_group_rect: Rect2
 var pause_movement: bool = false
+var alien_start_count: float = 0.0
+const COL: int = 6
+const ROW: int = 4
 
 func _ready() -> void:
-	for col in range(6):
+	for col in range(COL):
 		var column = Node2D.new()
 		column.name = "column%s" % str(col)
 		alien_group.add_child(column)
-		for row in range(4):
+		for row in range(ROW):
 			var alien := alien_scene.instantiate()
 			var spacing := Vector2(16,8)
 			alien.position = Vector2(
@@ -26,8 +31,9 @@ func _ready() -> void:
 			alien.position += Vector2(16,16)
 
 			column.add_child(alien)
+			alien_start_count += 1
 			alien.connect_move_signal(self)
-
+	
 func aliens_in_bottom_row() -> Array:
 	var bottom_row: Array = []
 	var aliens_alive := alien_group.get_children()
@@ -46,6 +52,7 @@ func move_aliens() -> void:
 		alien_group.position.y += 40
 		direction *= -1
 	
+	audio_stream_player.play()
 	alien_moved.emit()
 	aliens_in_bottom_row().pick_random().fire(projectile_manager)
 	
@@ -58,8 +65,10 @@ func is_new_position_on_screen(move_step) -> bool:
 	return true
 	
 func _on_timer_timeout() -> void:
-	if alien_group.get_child_count() > 0 and not pause_movement:
+	if get_alien_count() > 0 and not pause_movement:
 		move_aliens()
+		timer.start(get_alien_count()/alien_start_count)
+		audio_stream_player.pitch_scale = 1 + get_alien_count() / alien_start_count
 	
 func on_player_spawn() -> void:
 	await get_tree().create_timer(1).timeout
@@ -67,4 +76,12 @@ func on_player_spawn() -> void:
 	
 func on_player_hit() -> void:
 	pause_movement = true
+	
+func get_alien_count() -> float:
+	var alien_count: float = 0
+	for column in alien_group.get_children():
+		for alien in column.get_children():
+			if alien is Area2D:
+				alien_count += 1
+	return alien_count
 	
