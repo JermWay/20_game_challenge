3adc8395984fe761d819c63dc810d13912a80351
JermWay <jermway@gmail.com>
2025-08-21 11:03:26 -0500

    Switched to inertia based player ship movement

--- game_04_asteroids/scripts/player_ship.gd
+++ game_04_asteroids/scripts/player_ship.gd
@@ -1,12 +1,13 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var rotation_speed: float = 2.0
-@export var speed: float = 150.0
-@export var acceleration: float = 0.01
+@export var max_speed: float = 150.0
+@export var thrust_force: float = 200
 @export var bullet_pool: Node
 
-var is_accelerating: bool = false
+var is_thrusting: bool = false
 var is_rotating: bool = false
+var velocity: Vector2 = Vector2.ZERO
 
 @onready var thruster_sfx: AudioStreamPlayer = $ThrusterSFX
 @onready var fire_sfx: AudioStreamPlayer = $FireSFX
@@ -18,8 +19,8 @@ func _ready() -> void:
 	global_position = get_viewport_rect().get_center()
 	
 func _process(_delta: float) -> void:
-	if (is_accelerating or is_rotating) != thruster_sfx.playing:
-		thruster_sfx.playing = is_accelerating or is_rotating
+	if (is_thrusting or is_rotating) != thruster_sfx.playing:
+		thruster_sfx.playing = is_thrusting or is_rotating
 		
 func _physics_process(delta: float) -> void:
 	handle_fire()
@@ -28,15 +29,17 @@ func _physics_process(delta: float) -> void:
 	
 func handle_thrust(delta: float) -> void:
 	var facing_direction: Vector2 = Vector2.UP.rotated(rotation)
-	var target_velocity: Vector2 = Vector2.ZERO
-	if Input.is_action_pressed("thrust"):
-		target_velocity = facing_direction * speed
 	
-	is_accelerating = target_velocity != Vector2.ZERO
-	thruster_particles_center.emitting = is_accelerating
+	is_thrusting = Input.is_action_pressed("thrust")
+	thruster_particles_center.emitting = is_thrusting
 	
-	velocity = velocity.lerp(target_velocity, acceleration * delta)
-	move_and_slide()
+	if is_thrusting:
+		velocity += facing_direction * thrust_force * delta
+	
+	var speed = velocity.length()
+	if speed > max_speed:
+		velocity = velocity.normalized() * max_speed
+	position += velocity * delta
 	
 func handle_rotation(delta: float) -> void:
 	var left = Input.is_action_pressed("turn_left")
